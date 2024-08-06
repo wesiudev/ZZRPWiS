4e7fe7f689ef92491a8749d8393a43a35d8f7799
wesselpawel <wesselpawel@wp.pl>
2024-08-06 22:41:36 +0200

    homepage update

--- .history/src/components/Preambule_20240806224036.tsx
+++ .history/src/components/Preambule_20240806224036.tsx
@@ -11,15 +11,13 @@ export default function Preambule() {
       </h2>
       <div className="space-y-12">
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
-            1. CELEM ZWIĄZKU JEST:
-          </strong>{" "}
-          OBRONA GODNOŚCI, PRAW PRACUJĄCYCH, INTERESÓW MATERIALNYCH I
+          <strong className="text-4xl">1. CELEM ZWIĄZKU JEST:</strong> OBRONA
+          GODNOŚCI, PRAW PRACUJĄCYCH, INTERESÓW MATERIALNYCH I
           SOCJALNO-KULTUROWYCH, EMERYTÓW I RENCISTÓW, BEZROBOTNYCH I ICH RODZIN
           ORAZ INNYCH PODMIOTÓW ZWIĄZANYCH Z ROLNICTWEM.
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             2. STAŁE REPREZENTOWANIE POTRZEB ORAZ OBRONĘ INTERESÓW CZŁONKÓW
             ZWIĄZKU WOBEC ORGANÓW WŁADZY SZEROKO ROZUMIANEJ I GOSPODARKI
             POPRZEZ:
@@ -27,35 +25,28 @@ export default function Preambule() {
         </p>
         <ul className="list-disc list-inside ml-4 space-y-12">
           <li>
-            <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
-              A)
-            </strong>{" "}
-            DĄŻENIE DO ZAKAZU WSZELKIEJ DYSKRYMINACJI I MOBBINGU W STOSUNKACH
-            PRACY, SPRAWIEDLIWE TRAKTOWANIA PRACUJĄCYCH ZE STRONY PRACODAWCÓW,
-            ADMINISTRACJI RZĄDOWEJ I SAMORZĄDU TERYTORIALNEGO, ŚRODKI MASOWEGO
-            PRZEKAZU ORAZ RÓŻNYCH ORGANIZACJI I INSTYTUCJI,
+            <strong className="text-4xl">A)</strong> DĄŻENIE DO ZAKAZU WSZELKIEJ
+            DYSKRYMINACJI I MOBBINGU W STOSUNKACH PRACY, SPRAWIEDLIWE
+            TRAKTOWANIA PRACUJĄCYCH ZE STRONY PRACODAWCÓW, ADMINISTRACJI
+            RZĄDOWEJ I SAMORZĄDU TERYTORIALNEGO, ŚRODKI MASOWEGO PRZEKAZU ORAZ
+            RÓŻNYCH ORGANIZACJI I INSTYTUCJI,
           </li>
           <li>
-            <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
-              B)
-            </strong>{" "}
-            PRZEDKŁADANIE WNIOSKÓW, OPINII, ŻĄDAŃ NASZYCH CZŁONKÓW W KWESTII CEN
-            PRODUKTÓW ROLNYCH, CEN I ŚRODKÓW DO PRODUKCJI ROLNEJ, ZASAD SYSTEMU
-            ZBYTU PRODUKTÓW ROLNYCH, POLITYKĘ KREDYTOWĄ, PODATKOWĄ,
-            KULTURALNO-OŚWIATOWĄ I SOCJALNĄ, ŚWIADCZENIA EMERYTALNE ROLNIKÓW,
-            ZAKUPU ZIEMI I JEJ SPRZEDAŻY, PRZEKAZANIU, DAROWANIU,
+            <strong className="text-4xl">B)</strong> PRZEDKŁADANIE WNIOSKÓW,
+            OPINII, ŻĄDAŃ NASZYCH CZŁONKÓW W KWESTII CEN PRODUKTÓW ROLNYCH, CEN
+            I ŚRODKÓW DO PRODUKCJI ROLNEJ, ZASAD SYSTEMU ZBYTU PRODUKTÓW
+            ROLNYCH, POLITYKĘ KREDYTOWĄ, PODATKOWĄ, KULTURALNO-OŚWIATOWĄ I
+            SOCJALNĄ, ŚWIADCZENIA EMERYTALNE ROLNIKÓW, ZAKUPU ZIEMI I JEJ
+            SPRZEDAŻY, PRZEKAZANIU, DAROWANIU,
           </li>
           <li>
-            <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
-              C)
-            </strong>{" "}
-            INICJOWANIE, OPINIOWANIE I WSPÓŁUCZESTNICZENIE W OPRACOWYWANIU
-            PROJEKTÓW AKTÓW PRAWNYCH Z TEGO ZAKRESU W ODNIESIENIU DO WSI, GMINY,
-            POWIATU, WOJEWÓDZTWA, KRAJU.
+            <strong className="text-4xl">C)</strong> INICJOWANIE, OPINIOWANIE I
+            WSPÓŁUCZESTNICZENIE W OPRACOWYWANIU PROJEKTÓW AKTÓW PRAWNYCH Z TEGO
+            ZAKRESU W ODNIESIENIU DO WSI, GMINY, POWIATU, WOJEWÓDZTWA, KRAJU.
           </li>
         </ul>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             3. PODEJMOWANIE INTERWENCJI LUB REPREZENTOWANIE CZŁONKÓW W
             INDYWIDUALNYCH SPRAWACH CZŁONKÓW ZWIĄZKU ORAZ W PRZYPADKU ICH
             PRZEWLEKŁEGO LUB KRZYWDZĄCEGO ZAŁATWIANIA POPRZEZ ORGANY DO TEGO
@@ -63,21 +54,21 @@ export default function Preambule() {
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             4. BEZPOŚREDNIE ODDZIAŁYWANIE NA ROZWÓJ SPOŁECZNO-GOSPODARCZY
             REGIONU POPRZEZ SWOICH PRZEDSTAWICIELI WYBRANYCH DO LOKALNYCH
             ORGANÓW SAMORZĄDOWYCH, SEJMU, SENATU, EUROPARLAMENTU.
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             5. REPREZENTOWANIE I OBRONA INTERESÓW SPOŁECZNO-ZAWODOWYCH CZŁONKÓW
             ZWIĄZKU WOBEC ORGANÓW ADMINISTRACJI RZĄDOWEJ I SAMORZĄDOWEJ A TAKŻE
             INSTYTUCJI I ORGANIZACJI OBSŁUGUJĄCYCH WIEŚ I ROLNICTWO.
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             6. WSPÓŁDZIAŁANIE Z ORGANAMI WŁADZY I ADMINISTRACJI PAŃSTWOWEJ ORAZ
             PAŃSTWOWYMI I SPOŁECZNYMI JEDNOSTKAMI ORGANIZACYJNYMI OBSŁUGI
             ROLNICTWA I WSI W ISTOTNYCH SPRAWACH SPOŁECZNO-GOSPODARCZYCH I
@@ -85,7 +76,7 @@ export default function Preambule() {
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             7. WSPÓŁDZIAŁANIE Z IZBĄ ROLNICZĄ, ZWIĄZKAMI ROLNICZYCH ZRZESZEŃ
             BRANŻOWYCH, PARTIĄ I INNYMI PODMIOTAMI WE WSPÓLNYM REPREZENTOWANIU
             INTERESÓW CZŁONKÓW ZWIĄZKU, ROLNIKÓW INDYWIDUALNYCH I MIESZKAŃCÓW
@@ -93,14 +84,14 @@ export default function Preambule() {
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             8. UDZIELANIE POMOCY INSTRUKTAŻOWEJ I ORGANIZACYJNEJ, A W MIARĘ
             MOŻLIWOŚCI MATERIALNEJ ORGANIZACJOM W TYM MŁODZIEŻOWYM, PODMIOTOM,
             OSOBOM I INNYM WSPÓŁPRACUJĄCYM Z ZZRPWIS.
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             9. ZZRPWIS MOŻE UCZESTNICZYĆ W ORGANIZOWANIU WYPOCZYNKU WAKACYJNEGO
             DZIECI I MŁODZIEŻY, ROZWOJU SPORTU, TURYSTYKI, REKREACJI I
             DZIAŁALNOŚCI KULTURALNEJ NA WSI I W MIASTACH W KRAJU JAK I ZA
@@ -108,14 +99,14 @@ export default function Preambule() {
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             10. WSPÓŁPRACOWAĆ ZE WSZELKIMI PODMIOTAMI ORGANIZACJAMI,
             STOWARZYSZENIAMI, ZWIĄZKAMI W KRAJU JAKI I UE W CELU OSIĄGNIĘCIA
             CELÓW ZWIĄZKU I ZAKŁADAĆ ODDZIAŁY WŁASNE W KRAJACH UE.
           </strong>
         </p>
         <p>
-          <strong className="text-xl sm:text-2xl lg:text-3xl xl:text-4xl">
+          <strong className="text-4xl">
             11. REPREZENTOWANIE I OBRONA INTERESÓW PRACOWNICZYCH NA FORUM
             MIĘDZYNARODOWYM.
           </strong>
